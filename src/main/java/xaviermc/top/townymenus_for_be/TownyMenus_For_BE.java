5d1a138a295494699329a834a2a2cce2c31b5e16
CerealAxis <2234317768zyp@gamil.com>
2023-08-25 15:52:01 +0800

    新建项目

---
+++ src/main/java/xaviermc/top/townymenus_for_be/TownyMenus_For_BE.java
@@ -0,0 +1,17 @@
+package xaviermc.top.townymenus_for_be;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TownyMenus_For_BE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

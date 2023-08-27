627d676f3436ea4483c6deadd2e521fcfd1bac09
CerealAxis <2234317768zyp@gamil.com>
2023-08-27 17:43:17 +0800

    NotJoinedCityNationForm基本完成

--- src/main/java/xaviermc/top/townymenus_for_be/gui/NotJoinedCityNationForm.java
+++ src/main/java/xaviermc/top/townymenus_for_be/gui/NotJoinedCityNationForm.java
@@ -1,10 +1,8 @@
 package xaviermc.top.townymenus_for_be.gui;
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.geysermc.cumulus.CustomForm;
 import org.geysermc.cumulus.ModalForm;
 import org.geysermc.cumulus.SimpleForm;
@@ -17,8 +15,7 @@ import java.util.UUID;
 
 import static xaviermc.top.townymenus_for_be.gui.MainForm.sendMainForm;
 
-import xaviermc.top.townymenus_for_be.TownyMenus_For_BE;
-import xaviermc.top.townymenus_for_be.utils.*;
+import xaviermc.top.townymenus_for_be.utils.InputFormatChecker;
 
 public class NotJoinedCityNationForm {
     public static void sendNotJoinedCityNationForm(Player player) {
@@ -26,10 +23,10 @@ public class NotJoinedCityNationForm {
         if (!FloodgateApi.getInstance().isFloodgatePlayer(uuid)) return;
         FloodgateApi.getInstance().getPlayer(uuid).sendForm(
                 SimpleForm.builder()
-                        .title("加入/新建城市")
-                        .button("创建一个属于你的城市")
-                        .button("加入别人的城市")
-                        .button("返回上一页")
+                        .title(ChatColor.GREEN + "加入/新建城市")
+                        .button(ChatColor.YELLOW + "创建一个属于你的城市")
+                        .button(ChatColor.YELLOW + "加入别人的城市")
+                        .button(ChatColor.YELLOW + "返回上一页")
                         .responseHandler((f, r) -> {
                             SimpleFormResponse response = f.parseResponse(r);
                             if (response.isCorrect()) {
@@ -47,18 +44,33 @@ public class NotJoinedCityNationForm {
     }
 
     private static void sendJoinOtherCityForm(Player player) {
+        UUID uuid = player.getUniqueId();
+        FloodgateApi.getInstance().getPlayer(uuid).sendForm(
+                CustomForm.builder()
+                        .title(ChatColor.GREEN + "加入别人的城市")
+                        .input(ChatColor.YELLOW + "请输入城市名字", "请输入城市名字")
+                        .responseHandler((f, r) -> {
+                            CustomFormResponse response = f.parseResponse(r);
+                            String cityname = response.getInput(0);
+                            if (!InputFormatChecker.isValidCityName(cityname)) {
+                                player.sendMessage(ChatColor.RED + "城市名字只能包含英文和数字，将重新发送表单，请重新填写。");
+                                sendJoinOtherCityForm(player);
+                                return;
+                            }
+                            player.performCommand("town join " + cityname);
+                        })
+        );
     }
-
 
     private static void sendCreateOwnCityForm(Player player) {
         UUID uuid = player.getUniqueId();
         String townyadvanced_town_creation_cost = PlaceholderAPI.setPlaceholders(player, "%townyadvanced_town_creation_cost%");
         FloodgateApi.getInstance().getPlayer(uuid).sendForm(
                 ModalForm.builder()
-                        .title("创建属于你的城市")
-                        .content("是否选择创建你的城市，您的城市初始领地为您脚下所在区块 \n预计成本为" + townyadvanced_town_creation_cost)
-                        .button1("确认")
-                        .button2("取消")
+                        .title(ChatColor.GREEN + "创建属于你的城市")
+                        .content(ChatColor.YELLOW + "是否选择创建你的城市，您的城市初始领地为您脚下所在区块 \n预计成本为" + townyadvanced_town_creation_cost)
+                        .button1(ChatColor.GREEN + "确认")
+                        .button2(ChatColor.RED + "取消")
                         .responseHandler((f, r) -> {
                             ModalFormResponse response = f.parseResponse(r);
                             if (response.isCorrect()) {
@@ -78,14 +90,14 @@ public class NotJoinedCityNationForm {
         UUID uuid = player.getUniqueId();
         FloodgateApi.getInstance().getPlayer(uuid).sendForm(
                 CustomForm.builder()
-                        .title("创建城市")
-                        .input("请输入你的城市名字", "仅支持输入英文和数字")
+                        .title(ChatColor.GREEN + "创建城市")
+                        .input(ChatColor.YELLOW + "请输入你的城市名字", "仅支持输入英文和数字")
                         .responseHandler((form, responseData) -> {
                             CustomFormResponse response = form.parseResponse(responseData);
                             String cityName = response.getInput(0);
                             if (!InputFormatChecker.isValidCityName(cityName)) {
                                 player.sendMessage(ChatColor.RED + "城市名字只能包含英文和数字，将重新发送表单，请重新填写。");
-                                    sendCreateCityForm(player);
+                                sendCreateCityForm(player);
                                 return;
                             }
                             player.performCommand("town new " + cityName);
--- src/main/java/xaviermc/top/townymenus_for_be/utils/InputFormatChecker.java
+++ src/main/java/xaviermc/top/townymenus_for_be/utils/InputFormatChecker.java
@@ -2,6 +2,6 @@ package xaviermc.top.townymenus_for_be.utils;
 
 public class InputFormatChecker {
     public static boolean isValidCityName(String cityname){
-        return  cityname.matches("[a-zA-Z0-9]");
+        return  cityname.matches("[a-zA-Z0-9]+");
     }
 }
